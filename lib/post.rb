47ab4a26837d63296656f2e667fa7cd6703a65da
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-20 23:40:53 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,21 +3,5 @@ class Post
 
   @@all = []
 
-  def initialize(name, artist=nil)
-    @name = name
-    @artist = artist
-    @@all << self
-  end
-
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-  def self.all
-    @@all
-  end
-end
+ end
+ end
