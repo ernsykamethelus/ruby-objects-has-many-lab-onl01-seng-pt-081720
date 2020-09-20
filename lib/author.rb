c0145733694328f7e548c505308b94769c42c8db
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-20 23:42:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,26 +1,29 @@
 class Author 
-  attr_accessor :name, :posts
-  
+  attr_accessor :name
+  @@post_count = 0
+
   def initialize(name)
     @name = name
-end
+    @posts = []
+  end
 
- def posts 
-    Post.all.select do |post|
-      post.author == self
-    end
+  def posts
+    @posts
   end
 
   def add_post(post)
+    @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
-    add_title(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def self.post_count
-    Post.all.count
+    @@post_count
   end
-end
